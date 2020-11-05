c94af905b1c0b8d0e129da8a608c9ac3e7c78a9f
Bob Myhill <myhill.bob@gmail.com>
2020-11-05 22:38:23 +0000

    added strain rate and deriv function to damped dp

--- include/aspect/material_model/rheology/drucker_prager.h
+++ include/aspect/material_model/rheology/drucker_prager.h
@@ -99,6 +99,16 @@ namespace aspect
                              const double max_yield_stress) const;
 
           /**
+           * Compute the strain rate and first stress derivative
+           * as a function of stress based on the damped Drucker-Prager flow law.
+           */
+          std::pair<double, double>
+          compute_strain_rate_and_derivative (const double stress,
+                                              const double pressure,
+                                              const unsigned int composition,
+                                              const DruckerPragerParameters p) const;
+
+          /**
            * Compute the derivative of the plastic viscosity with respect to pressure.
            */
           double
